3c95d40a2a2764a822d6d2adf6eb3c7d1938c143
loriluo95 <zluo2@wpi.edu>
2017-02-03 09:37:38 -0500

    Add files via upload

--- phase2-testcall/example2.c
+++ phase2-testcall/example2.c
@@ -1,8 +1,6 @@
 #include <sys/syscall.h>
 #include <stdio.h>
-#define __NR_cs3013_syscall1 378
 #define __NR_cs3013_syscall2 378
-#define __NR_cs3013_syscall3 378
 
 struct ancestry{
 	pid_t ancestor[10];
@@ -10,11 +8,20 @@ struct ancestry{
 	pid_t children[100];
 };
 
-long testcall(pid_t *target_pid, struct ancestry *response){	
-	return(long)syscall(__NR_cs3013_syscall2, target_pid, response);; 
+long testcall(unsigned short *target_pid, struct ancestry *response){	
+	return(long)syscall(__NR_cs3013_syscall2, target_pid, response);
 }
 int main(){
+	/* Allocate memory*/
+	unsigned short *target_pid = (unsigned short*)malloc(sizeof(unsigned short));
 	struct ancestry* ancestryTest = malloc(sizeof(ancestry)); 
-	int pid = fork(); 
+	printf("\tcs3013_syscall2: %ld\n", testcall());
+	printf("Target ID: %d\n", (int)target_pid);
+	printf("Ancestor: %d\n", (int)ancestry->ancestor);
+	printf("Siblings: %d\n", (int)ancestry->siblings);
+	printf("Children:  %d\n", (int)ancestry->children);
 
+	free(target_pid);
+	free(ancestryTest);
+	return 0;
 }
