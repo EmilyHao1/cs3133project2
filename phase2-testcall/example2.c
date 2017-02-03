140a43ce5d49d1dc77f468e66e9c2941bc11c2e9
EmilyHao1 <emilyhao1314@gmail.com>
2017-02-02 22:56:21 -0500

    ya

--- phase2-testcall/example2.c
+++ phase2-testcall/example2.c
@@ -1,6 +1,8 @@
 #include <sys/syscall.h>
 #include <stdio.h>
-
+#define __NR_cs3013_syscall1 378
+#define __NR_cs3013_syscall2 378
+#define __NR_cs3013_syscall3 378
 
 struct ancestry{
 	pid_t ancestor[10];
@@ -8,12 +10,11 @@ struct ancestry{
 	pid_t children[100];
 };
 
-int testcall(){
-	new_sys_cs3013_syscall2(unsigned short *target_pid, struct ancestry *response);	
-	return 0; 
+long testcall(pid_t *target_pid, struct ancestry *response){	
+	return(long)syscall(__NR_cs3013_syscall2, target_pid, response);; 
 }
 int main(){
-
-
+	struct ancestry* ancestryTest = malloc(sizeof(ancestry)); 
+	int pid = fork(); 
 
 }
